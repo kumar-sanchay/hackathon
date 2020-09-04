8465365558d1eed8c2383e47dc47141869d3bd1b
kumar-sanchay <dsanchaykumar@gmail.com>
2020-09-04 16:31:45 +0530

    first commit

--- hackathon_project/settings.py
+++ hackathon_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hackathon_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'av_chatbot',
+        'USER': 'chatbotuser',
+        'PASSWORD': 'Autovista1243',
+        'HOST': '127.0.0.1',
+        'PORT': ''
     }
 }
 
